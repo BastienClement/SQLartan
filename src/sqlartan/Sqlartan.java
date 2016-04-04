1ea7f6b83a1acd4ff7b0c3b9f727c5302415833a
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2016-04-04 15:39:02 +0200

    Added package view for GUI

--- src/sqlartan/Sqlartan.java
+++ src/sqlartan/Sqlartan.java
@@ -1,23 +1,44 @@
 package sqlartan;
 
-import java.sql.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Sqlartan {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+public class Sqlartan extends Application{
+
+	private Stage primaryStage;
+	private BorderPane mainLayout;
 
 	/**
-	 * Returns the current SQLite version.
+	 * The main entry point for all JavaFX applications.
+	 * The start method is called after the init method has returned,
+	 * and after the system is ready for the application to begin running.
 	 *
-	 * @return
-	 * @throws SQLException
+	 * <p>
+	 * NOTE: This method is called on the JavaFX Application Thread.
+	 * </p>
+	 *
+	 * @param primaryStage the primary stage for this application, onto which
+	 *                     the application scene can be set. The primary stage will be embedded in
+	 *                     the browser if the application was launched as an applet.
+	 *                     Applications may create other stages, if needed, but they will not be
+	 *                     primary stages and will not be embedded in the browser.
 	 */
-	public static String sqliteVersion() throws SQLException {
-		try (Connection connection = DriverManager.getConnection("jdbc:sqlite::memory:");
-		     Statement statement = connection.createStatement();
-		     ResultSet rs = statement.executeQuery("select sqlite_version();")) {
-			return rs.getString(1);
-		}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("SQLartan");
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Sqlartan.class.getResource("view/Sqlartan.fxml"));
+		mainLayout = loader.load();
+
+		primaryStage.setScene(new Scene(mainLayout));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
---
+++ src/sqlartan/view/SqlartanController.java
@@ -0,0 +1,8 @@
+package sqlartan.view;
+
+/**
+ * Created by guillaume on 04.04.16.
+ */
+public class SqlartanController {
+
+}
