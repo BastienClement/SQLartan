631fb16fff69174f8e176c7d88a6ab657971b728
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-04-17 21:51:51 +0200

    Rearange for proper treeview

--- src/sqlartan/view/SqlartanController.java
+++ src/sqlartan/view/SqlartanController.java
@@ -1,9 +1,6 @@
 package sqlartan.view;
 
-import javafx.beans.Observable;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
@@ -11,10 +8,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import sqlartan.Sqlartan;
+import sqlartan.core.AttachedDatabase;
+import sqlartan.core.Column;
 import sqlartan.core.Database;
 import sqlartan.core.Table;
-import sqlartan.core.util.IterableStream;
 import java.sql.SQLException;
+import java.util.Optional;
 
 /**
  * Created by guillaume on 04.04.16.
@@ -22,78 +21,95 @@ import java.sql.SQLException;
 public class SqlartanController {
 
 	private Sqlartan sqlartan;
-
-	public Database dataBase;
-
 	private ObservableList<testClass> rows = FXCollections.observableArrayList();
-
-
-	public void setApp(Sqlartan sqlartan)
-	{
-		this.sqlartan = sqlartan;
-	}
-
 	@FXML
 	private TreeView<String> treeView;
-
 	@FXML
-	private TableView table;
-
+	private TableView table = new TableView();
+	public void setApp(Sqlartan sqlartan) {
+		this.sqlartan = sqlartan;
+	}
 	@FXML
 	private void initialize() throws SQLException {
+		Database db = new Database("testdb.db");
+		tree(db);
+		treeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			try {
+				Optional<Table> oTable = db.table(newValue.getValue());
+				if (oTable.isPresent()) {
+					Table t = db.table(newValue.getValue()).get();
+					structure(t);
+				}
+			} catch (SQLException e) { // pas une table
 
-		dataBase = new Database("testdb.db");
+			}
+			System.out.println("Selected : " + newValue.getValue());
+		});
+		// Table t = db.table("table1").get();
+		//structure(t);
 
-		test();
 	}
 
-
-	void test() throws SQLException {
-		TreeItem<String> mainTreeItem = new TreeItem<String> ("Bases de données"); // Hidden
-		treeView.setShowRoot(false);
-
-		TreeItem<String> dbTreeItem1 =new TreeItem<String>(dataBase.name());
-		mainTreeItem.getChildren().add(dbTreeItem1);
-
-
-
-		for (Table table : dataBase.tables())
-		{
-			dbTreeItem1.getChildren().add(new TreeItem<String>(table.name()));
-		}
-
-		TreeItem<String> dbTreeItem2 =new TreeItem<String>("Une autre base de données");
-
-
-		treeView.setRoot(mainTreeItem);
-		mainTreeItem.getChildren().add(dbTreeItem2);
-
+	/**
+	 * Create the tree for a specific database
+	 *
+	 * @param database the database
+	 * @throws SQLException
+	 */
+	void tree(Database database) throws SQLException {
+		TreeItem<String> mainTreeItem = new TreeItem<>("Bases de données"); // Hidden
 		mainTreeItem.setExpanded(true);
 
+		treeView.setShowRoot(false);
+		treeView.setRoot(mainTreeItem);
 
-		dbTreeItem2.getChildren().add(new TreeItem<String>("Une table"));
-		dbTreeItem2.getChildren().add(new TreeItem<String>("Une vue"));
+		// Main
+		TreeItem<String> trees = new TreeItem<>(database.name());
+		for (Table table : database.tables())
+			trees.getChildren().add(new TreeItem<>(table.name()));
+		/* TODO remove when view is coded
+		for (View view : database.views())
+			trees.getChildren().add(new TreeItem<>(view.name()));
+		*/
+		mainTreeItem.getChildren().add(trees);
 
-
-
-	}
-
-	void test2()
-	{
-		table.setItems(rows);
-
-		testClass tmp = rows.get(0);
-
-		for (int i = 0; i < 10; ++i)
-		{
-			TableColumn<testClass, String> coll = new  TableColumn<testClass, String>();
-			table.getColumns().add(coll);
-			coll.setCellValueFactory(cellDate->cellDate.getValue().tab.get(i));
-
+		// Attached database
+		for (AttachedDatabase adb : database.attached().values()) {
+			TreeItem<String> tItems = new TreeItem<>(adb.name());
+			for (Table table : adb.tables())
+				tItems.getChildren().add(new TreeItem<>(table.name()));
+			/* TODO remove when view is coded
+			for (View view : database.views())
+				tItems.getChildren().add(new TreeItem<>(view.name()));
+			*/
+			mainTreeItem.getChildren().add(tItems);
 		}
+	}
 
+	void structure(Table t) {
+		table.setEditable(true);
 
+		for (Column c : t.columns()) {
+			TableColumn tc = new TableColumn(c.name());
+			table.getColumns().add(tc);
+		}
+		table.setVisible(true);
 
 	}
+
+	/*
+	void test2() {
+		table.setItems(rows);
+		testClass tmp = rows.get(0);
+		rows.add
+
+		for (int i = 0; i < 10; ++i) {
+			TableColumn<testClass, String> coll = new TableColumn<>();
+			table.getColumns().add(coll);
+			coll.setCellValueFactory(cellDate -> cellDate.getValue().tab.get(i));
+		}
+	}
+	*/
+
 
 }
--- src/sqlartan/view/testClass.java
+++ src/sqlartan/view/testClass.java
@@ -1,25 +1,17 @@
 package sqlartan.view;
 
 import javafx.beans.property.StringProperty;
-import jdk.nashorn.internal.objects.ArrayBufferView;
 import java.util.ArrayList;
 
 /**
  * Created by julien on 13.04.16.
  */
 public class testClass {
-	ArrayList<StringProperty> tab;
+	ArrayList<StringProperty> tab = new ArrayList<>();
 
-	public testClass()
-	{
-		tab = new ArrayList<StringProperty>();
-	}
-
-	public void add(StringProperty sp)
-	{
+	public void add(StringProperty sp) {
 		tab.add(sp);
 	}
-
 
 
 }
