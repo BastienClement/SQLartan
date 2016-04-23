f9d7df83e81aea3617b727298605c015fe3c0177
Bastien Clément <g@ledric.me>
2016-04-23 15:02:26 +0200

    Initial support for ATTACH/DETACH

--- src/sqlartan/core/AttachedDatabase.java
+++ src/sqlartan/core/AttachedDatabase.java
@@ -1,17 +1,37 @@
 package sqlartan.core;
 
+import sqlartan.core.util.RuntimeSQLException;
 import java.io.File;
 import java.sql.SQLException;
 
 public class AttachedDatabase extends Database {
 	private Database main;
 
-	AttachedDatabase(File path, String name, Database main) throws SQLException {
-		super(path, name);
+	AttachedDatabase(Database main, File path, String name) throws SQLException {
+		super(path, name, main.connection);
 		this.main = main;
 	}
 
 	public Database main() {
 		return main;
+	}
+
+	public void detach() {
+		try {
+			main.detach(name());
+		} catch (SQLException e) {
+			throw new RuntimeSQLException(e);
+		}
+	}
+
+	@Override
+	public void close() {
+		// We must not call .close() on the Connection from a child Database
+		// Instead detach self
+		try {
+			assemble("DETACH DATABASE ", name()).execute();
+		} catch (SQLException e) {
+			throw new RuntimeSQLException(e);
+		}
 	}
 }
