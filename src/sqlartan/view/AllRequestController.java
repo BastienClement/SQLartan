16882bfd29f0f70dcee79485ec356aaac52b9bf6
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-04-29 10:41:31 +0200

    Add basic fxml for the all gui request

---
+++ src/sqlartan/view/AllRequestController.java
@@ -0,0 +1,21 @@
+package sqlartan.view;
+/*
+ * Projet : SQLartan
+ * Créé le 29.04.2016.
+ * Auteur : Adriano Ruberto
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AllRequestController {
+	@FXML
+	Button execute;
+
+
+	public void toast() {
+		System.out.println("Coucou");
+
+	}
+
+}
