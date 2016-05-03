8d738461910bca71c572bd65c9bb329734f7f1e8
galedric <g@ledric.me>
2016-05-03 16:19:24 +0200

    ANALYZE statement

--- src/sqlartan/core/ast/AnalyzeStatement.java
+++ src/sqlartan/core/ast/AnalyzeStatement.java
@@ -1,9 +1,42 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.SQLBuilder;
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.ANALYZE;
+import static sqlartan.core.ast.token.Operator.DOT;
 
-public abstract class AnalyzeStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_analyze.html
+ */
+public class AnalyzeStatement implements Statement {
+	public String schema;
+	public String subject;
+	public boolean ambiguous;
+
 	public static Statement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		context.consume(ANALYZE);
+		AnalyzeStatement analyze = new AnalyzeStatement();
+		if (context.next(DOT)) {
+			analyze.schema = context.consumeIdentifier().value;
+			context.consume(DOT);
+			analyze.subject = context.consumeIdentifier().value;
+			analyze.ambiguous = false;
+		} else {
+			analyze.schema = analyze.subject = context.consumeIdentifier().value;
+			analyze.ambiguous = true;
+		}
+		return analyze;
+	}
+
+	@Override
+	public void toSQL(SQLBuilder sql) {
+		sql.append("ANALYZE ");
+		if (ambiguous || schema == null) {
+			sql.appendIdentifier(subject);
+		} else {
+			sql.appendIdentifier(schema)
+			   .append(".")
+			   .appendIdentifier(subject);
+		}
 	}
 }
