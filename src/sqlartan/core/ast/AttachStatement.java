968b37aba3b0053fd19bf26256b879020033b98b
galedric <g@ledric.me>
2016-05-03 15:57:29 +0200

    ATTACH statement

--- src/sqlartan/core/ast/AttachStatement.java
+++ src/sqlartan/core/ast/AttachStatement.java
@@ -1,9 +1,24 @@
 package sqlartan.core.ast;
 
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.AS;
+import static sqlartan.core.ast.token.Keyword.ATTACH;
+import static sqlartan.core.ast.token.Keyword.DATABASE;
 
-public abstract class AttachStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_attach.html
+ */
+public class AttachStatement implements Statement {
+	public Expression file;
+	public String schema;
+
 	public static AttachStatement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		context.consume(ATTACH);
+		context.tryConsume(DATABASE);
+		AttachStatement attach = new AttachStatement();
+		attach.file = context.parse(Expression::parse);
+		context.consume(AS);
+		attach.schema = context.consumeIdentifier().value;
+		return attach;
 	}
 }
