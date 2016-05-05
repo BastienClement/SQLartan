45d650f4a9d752bae457fcbd8fd3c06e9464c21b
Bastien Clément <g@ledric.me>
2016-05-05 14:24:57 +0200

    BEGIN

--- src/sqlartan/core/ast/BeginStatement.java
+++ src/sqlartan/core/ast/BeginStatement.java
@@ -1,9 +1,48 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.SQLBuilder;
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.*;
+import static sqlartan.util.Matching.match;
 
-public abstract class BeginStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_transaction.html
+ */
+public class BeginStatement implements Statement {
+	public enum Mode {
+		None,
+		Deferred,
+		Immediate,
+		Exclusive
+	}
+
+	public Mode mode = Mode.None;
+
 	public static BeginStatement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		context.consume(BEGIN);
+
+		BeginStatement begin = new BeginStatement();
+		begin.mode = match(context.current())
+			.when(DEFERRED, () -> Mode.Deferred)
+			.when(IMMEDIATE, () -> Mode.Immediate)
+			.when(EXCLUSIVE, () -> Mode.Exclusive)
+			.orElse(Mode.None);
+
+		if (begin.mode != Mode.None) {
+			context.consume();
+		}
+
+		context.tryConsume(TRANSACTION);
+		return begin;
+	}
+
+	@Override
+	public void toSQL(SQLBuilder sql) {
+		sql.append("BEGIN")
+		   .append(match(mode)
+			   .when(Mode.Deferred, () -> " DEFERRED")
+			   .when(Mode.Immediate, () -> " IMMEDIATE")
+			   .when(Mode.Exclusive, () -> " EXCLUSIVE")
+			   .orElse(""));
 	}
 }
