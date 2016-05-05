6eb2003cfc4aa920ce62ab55a984cd6b0b602756
Bastien Clément <g@ledric.me>
2016-05-05 14:25:50 +0200

    DETACH

--- src/sqlartan/core/ast/DetachStatement.java
+++ src/sqlartan/core/ast/DetachStatement.java
@@ -1,9 +1,25 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.SQLBuilder;
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.*;
 
-public abstract class DetachStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_detach.html
+ */
+public class DetachStatement implements Statement {
+	public String schema;
+
 	public static DetachStatement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		context.consume(DETACH);
+		context.tryConsume(DATABASE);
+		DetachStatement detach = new DetachStatement();
+		detach.schema = context.consumeIdentifier().value;
+		return detach;
+	}
+
+	@Override
+	public void toSQL(SQLBuilder sql) {
+		sql.append("DETACH ").appendIdentifier(schema);
 	}
 }
