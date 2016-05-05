742a94c84fcf15895292764bdadfa0caa5b8fbf7
Bastien Clément <g@ledric.me>
2016-05-05 14:35:03 +0200

    RELEASE

--- src/sqlartan/core/ast/ReleaseStatement.java
+++ src/sqlartan/core/ast/ReleaseStatement.java
@@ -1,9 +1,29 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.SQLBuilder;
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.RELEASE;
+import static sqlartan.core.ast.token.Keyword.SAVEPOINT;
 
-public abstract class ReleaseStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_savepoint.html
+ */
+public class ReleaseStatement implements Statement {
+	public String savepoint;
+
+	public ReleaseStatement() {}
+	public ReleaseStatement(String savepoint) {
+		this.savepoint = savepoint;
+	}
+
 	public static Statement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		context.consume(RELEASE);
+		context.tryConsume(SAVEPOINT);
+		return new ReleaseStatement(context.consumeIdentifier().value);
+	}
+
+	@Override
+	public void toSQL(SQLBuilder sql) {
+		sql.append("RELEASE ").appendIdentifier(savepoint);
 	}
 }
