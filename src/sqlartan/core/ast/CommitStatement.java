838678b3dcdc783e363eebdc84745c71af905b7f
Bastien Clément <g@ledric.me>
2016-05-05 14:25:02 +0200

    COMMIT

--- src/sqlartan/core/ast/CommitStatement.java
+++ src/sqlartan/core/ast/CommitStatement.java
@@ -1,9 +1,29 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.SQLBuilder;
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.COMMIT;
+import static sqlartan.core.ast.token.Keyword.END;
+import static sqlartan.core.ast.token.Keyword.TRANSACTION;
 
-public abstract class CommitStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_transaction.html
+ */
+public class CommitStatement implements Statement {
+	public static final CommitStatement instance = new CommitStatement();
+
 	public static CommitStatement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		if (!context.tryConsume(COMMIT)) {
+			context.consume(END);
+		}
+		context.tryConsume(TRANSACTION);
+		return instance;
+	}
+
+	private CommitStatement() {}
+
+	@Override
+	public void toSQL(SQLBuilder sql) {
+		sql.append("COMMIT");
 	}
 }
