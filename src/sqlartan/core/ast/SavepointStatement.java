2fa1aad16d40bfc260cd3cff20624b1361d15264
Bastien Clément <g@ledric.me>
2016-05-05 14:33:27 +0200

    SAVEPOINT

--- src/sqlartan/core/ast/SavepointStatement.java
+++ src/sqlartan/core/ast/SavepointStatement.java
@@ -1,9 +1,27 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.SQLBuilder;
 import sqlartan.core.ast.parser.ParserContext;
+import static sqlartan.core.ast.token.Keyword.SAVEPOINT;
 
-public abstract class SavepointStatement implements Statement {
+/**
+ * https://www.sqlite.org/lang_savepoint.html
+ */
+public class SavepointStatement implements Statement {
+	public String savepoint;
+
+	public SavepointStatement() {}
+	public SavepointStatement(String savepoint) {
+		this.savepoint = savepoint;
+	}
+
 	public static SavepointStatement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		context.consume(SAVEPOINT);
+		return new SavepointStatement(context.consumeIdentifier().value);
+	}
+
+	@Override
+	public void toSQL(SQLBuilder sql) {
+		sql.append("SAVEPOINT ").appendIdentifier(savepoint);
 	}
 }
