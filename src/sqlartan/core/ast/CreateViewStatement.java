f930f85d674f6647dd7fc0490de4e21d7b57f96b
Bastien Clément <g@ledric.me>
2016-05-07 00:59:52 +0200

    CREATE VIEW

--- src/sqlartan/core/ast/CreateViewStatement.java
+++ src/sqlartan/core/ast/CreateViewStatement.java
@@ -1,12 +1,53 @@
 package sqlartan.core.ast;
 
+import sqlartan.core.ast.gen.Builder;
 import sqlartan.core.ast.parser.ParserContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import static sqlartan.core.ast.Keyword.*;
+import static sqlartan.core.ast.Operator.LEFT_PAREN;
+import static sqlartan.core.ast.Operator.RIGHT_PAREN;
 
 /**
  * https://www.sqlite.org/lang_createview.html
  */
+@SuppressWarnings({ "OptionalUsedAsFieldOrParameterType", "WeakerAccess" })
 public class CreateViewStatement extends CreateStatement {
+	public boolean temporary;
+	public boolean ifNotExists;
+	public Optional<String> schema;
+	public String name;
+	public List<String> columns = new ArrayList<>();
+	public SelectStatement as;
+
 	public static CreateViewStatement parse(ParserContext context) {
-		throw new UnsupportedOperationException();
+		CreateViewStatement create = new CreateViewStatement();
+		context.consume(CREATE);
+		create.temporary = context.tryConsume(TEMP) || context.tryConsume(TEMPORARY);
+		context.consume(VIEW);
+		create.ifNotExists = context.tryConsume(IF, NOT, EXISTS);
+		create.schema = context.optConsumeSchema();
+		create.name = context.consumeIdentifier();
+		if (context.tryConsume(LEFT_PAREN)) {
+			create.columns = context.parseList(ParserContext::consumeIdentifier);
+			context.consume(RIGHT_PAREN);
+		}
+		context.consume(AS);
+		create.as = SelectStatement.parse(context);
+		return create;
+	}
+
+	@Override
+	public void toSQL(Builder sql) {
+		sql.append(CREATE);
+		if (temporary) sql.append(TEMPORARY);
+		sql.append(VIEW);
+		if (ifNotExists) sql.append(IF, NOT, EXISTS);
+		schema.ifPresent(sql::appendSchema);
+		if (!columns.isEmpty()) {
+			sql.append(LEFT_PAREN).appendIdentifiers(columns).append(RIGHT_PAREN);
+		}
+		sql.append(AS).append(as);
 	}
 }
