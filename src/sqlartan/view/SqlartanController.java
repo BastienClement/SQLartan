12d06517a346bdd55e17eefac25fa18568b6b084
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-05-15 18:38:23 +0200

    Added menu item and link to the function

--- src/sqlartan/view/SqlartanController.java
+++ src/sqlartan/view/SqlartanController.java
@@ -19,12 +19,12 @@ import sqlartan.view.tabs.TableTabsController;
 import sqlartan.view.treeitem.*;
 import sqlartan.view.util.Popup;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-import static sqlartan.util.Matching.match;
 
 /**
  * Created by guillaume on 04.04.16.
@@ -218,7 +218,7 @@ public class SqlartanController {
 				ButtonType buttonRetry = new ButtonType("Retry");
 				ButtonType buttonNewFile = new ButtonType("Choose new");
 				Optional<ButtonType> res = Popup.warning("Problem while opening database", "Error: " + e.getMessage(),
-						buttonCancel, buttonRetry, buttonNewFile);
+					buttonCancel, buttonRetry, buttonNewFile);
 				if (res.isPresent()) {
 					if (res.get() == buttonNewFile)
 						file = openSQLLiteDB();
@@ -307,6 +307,7 @@ public class SqlartanController {
 		// Create the file popup
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open SQLite database");
+
 		File file = fileChooser.showOpenDialog(sqlartan.getPrimaryStage());
 		Database tmpDB = null;
 
@@ -451,13 +452,40 @@ public class SqlartanController {
 		database.importFromString(sql);
 	}
 
+	@FXML
+	public void importFX() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Import SQLite database");
+		try {
+			db.importfromFile(fileChooser.showOpenDialog(sqlartan.getPrimaryStage()));
+		} catch (SQLException | IOException e) {
+			Popup.error(":(", e.getMessage());
+		}
+	}
+
 	/**
-	 * Export a database
+	 * Export the database
 	 *
-	 * @param database
+	 * @throws SQLException
 	 */
-	public String export(Database database) throws SQLException {
-		return database.export();
+	@FXML
+	public void export() throws SQLException {
+		FileChooser fileChooser = new FileChooser();
+
+		//Set extension filter
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("SQL files (*.sql)", "*.sql");
+		fileChooser.getExtensionFilters().add(extFilter);
+
+		//Show save file dialog
+		File file = fileChooser.showSaveDialog(sqlartan.getPrimaryStage());
+
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write(db.export());
+			fileWriter.close();
+		} catch (IOException ignored) {
+
+		}
 	}
 
 }
