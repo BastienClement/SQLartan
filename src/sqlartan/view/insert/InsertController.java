ce9fdecdd8c50809d8251534c8d781c608e0b4ac
Julien Leroy <julien.leroy@limayankee.com>
2016-05-20 20:09:36 +0200

    insert

--- src/sqlartan/view/insert/InsertController.java
+++ src/sqlartan/view/insert/InsertController.java
@@ -1,7 +1,14 @@
 package sqlartan.view.insert;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 /**
  * Created by julien on 20.05.16.
  */
 public class InsertController {
+	@FXML private Button submit;
+
+
+
 }
