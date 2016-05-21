a804f5ea39698f62039d119eac371ecfc50f8944
Julien Leroy <julien@imac-de-julien.home>
2016-05-21 12:19:56 +0200

    about update

--- src/sqlartan/view/about/About.java
+++ src/sqlartan/view/about/About.java
@@ -1,7 +1,19 @@
 package sqlartan.view.about;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 /**
  * Created by julien on 20.05.16.
  */
 public class About {
+
+	@FXML
+	private TextArea description;
+
+	@FXML
+	private void initialize() {
+		description.setFocusTraversable(false);
+		description.setMouseTransparent(true);
+	}
 }
