16bb5ce60db8fdd19906d2d65008212e233a1c93
Julien Leroy <julien@imac-de-julien.home>
2016-05-21 14:20:03 +0200

    Formulaire insertion partie gui fini

---
+++ src/sqlartan/view/tabs/InsertRawStructure.java
@@ -0,0 +1,14 @@
+package sqlartan.view.tabs;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by julien on 21.05.16.
+ */
+public class InsertRawStructure {
+	StringProperty name;
+	StringProperty type;
+	StringProperty value;
+
+
+}
