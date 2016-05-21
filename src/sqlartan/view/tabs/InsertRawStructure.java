ad20389c3a156256fb7732cb3f8e8eb9329e4a7a
Julien Leroy <julien@imac-de-julien.home>
2016-05-21 14:20:37 +0200

    modification de la classe insertraw structure

--- src/sqlartan/view/tabs/InsertRawStructure.java
+++ src/sqlartan/view/tabs/InsertRawStructure.java
@@ -1,6 +1,8 @@
 package sqlartan.view.tabs;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import sqlartan.core.Column;
 
 /**
  * Created by julien on 21.05.16.
@@ -11,4 +13,14 @@ public class InsertRawStructure {
 	StringProperty value;
 
 
+	public InsertRawStructure(Column column){
+		this.name = new SimpleStringProperty(column.name());
+		this.type = new SimpleStringProperty(column.type());
+		this.value = new SimpleStringProperty();
+
+	}
+
+	public void setValue(String str){
+		value = new SimpleStringProperty(str);
+	}
 }
