96504bf4a3dbc54c84050d3b48a839ad46d54a72
Antoine Drabble <antoine.drabble@gmail.com>
2016-05-22 10:23:11 +0200

    Add two drop and rename buttons on the database tab

--- src/sqlartan/view/tabs/DatabaseTabsController.java
+++ src/sqlartan/view/tabs/DatabaseTabsController.java
@@ -2,13 +2,12 @@ package sqlartan.view.tabs;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.util.Callback;
 import sqlartan.Sqlartan;
 import sqlartan.core.Database;
 import sqlartan.view.tabs.struct.DatabaseStructure;
@@ -27,7 +26,9 @@ public class DatabaseTabsController extends TabsController {
 	@FXML
 	private TableColumn<DatabaseStructure, String> colLignes;
 	@FXML
-	private TableColumn<DatabaseStructure, String> colActions;
+	private TableColumn<DatabaseStructure, String> colRename;
+	@FXML
+	private TableColumn<DatabaseStructure, String> colDelete;
 	@FXML
 	private TabPane tabPane;
 
@@ -54,9 +55,86 @@ public class DatabaseTabsController extends TabsController {
 		});
 
 		colName.setCellValueFactory(param -> param.getValue().nameProperty());
-		colActions.setCellValueFactory(param -> param.getValue().actionsProperty());
 		colLignes.setCellValueFactory(param -> param.getValue().lignesProperty());
 		colType.setCellValueFactory(param -> param.getValue().typeProperty());
+		colRename.setCellFactory(new Callback<TableColumn<DatabaseStructure, String>, TableCell<DatabaseStructure, String>>() {
+			@Override
+			public TableCell call( final TableColumn<DatabaseStructure, String> param )
+			{
+				final TableCell<DatabaseStructure, String> cell = new TableCell<DatabaseStructure, String>()
+				{
+					final Button btn = new Button( "Rename" );
+
+					@Override
+					public void updateItem( String item, boolean empty )
+					{
+						super.updateItem( item, empty );
+						if ( empty )
+						{
+							setGraphic( null );
+							setText( null );
+						}
+						else
+						{
+							btn.setOnAction( ( ActionEvent event ) ->
+							{
+								DatabaseStructure dbStruct = getTableView().getItems().get( getIndex() );
+								switch(dbStruct.typeProperty().get()){
+									case "View" :
+										database.view(dbStruct.nameProperty().get());
+										break;
+									case "Table" :
+										database.table(dbStruct.nameProperty().get()).get();
+										break;
+								}
+							} );
+							setGraphic( btn );
+							setText( null );
+						}
+					}
+				};
+				return cell;
+			}
+		});
+		colDelete.setCellFactory(new Callback<TableColumn<DatabaseStructure, String>, TableCell<DatabaseStructure, String>>() {
+			@Override
+			public TableCell call( final TableColumn<DatabaseStructure, String> param )
+			{
+				final TableCell<DatabaseStructure, String> cell = new TableCell<DatabaseStructure, String>()
+				{
+					final Button btn = new Button( "Drop" );
+
+					@Override
+					public void updateItem( String item, boolean empty )
+					{
+						super.updateItem( item, empty );
+						if ( empty )
+						{
+							setGraphic( null );
+							setText( null );
+						}
+						else
+						{
+							btn.setOnAction( ( ActionEvent event ) ->
+							{
+								DatabaseStructure dbStruct = getTableView().getItems().get( getIndex() );
+								switch(dbStruct.typeProperty().get()){
+									case "View" :
+										database.view(dbStruct.nameProperty().get()).get().drop();
+										break;
+									case "Table" :
+										database.table(dbStruct.nameProperty().get()).get().drop();
+										break;
+								}
+							} );
+							setGraphic( btn );
+							setText( null );
+						}
+					}
+				};
+				return cell;
+			}
+		});
 
 		tabPane.getSelectionModel().clearSelection();
 	}
