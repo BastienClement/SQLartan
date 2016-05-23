781ce09e48a1467517e657f8eccdf57a3a9e8f5f
Adriano Ruberto <adriano.ruberto@heig-vd.ch>
2016-05-23 22:13:17 +0200

    Remove warnings

--- src/sqlartan/view/tabs/DatabaseTabsController.java
+++ src/sqlartan/view/tabs/DatabaseTabsController.java
@@ -10,20 +10,16 @@ import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 import sqlartan.Sqlartan;
 import sqlartan.core.Database;
+import sqlartan.view.AllRequestController;
 import sqlartan.view.SqlartanController;
 import sqlartan.view.tabs.struct.DatabaseStructure;
 import sqlartan.view.util.Popup;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import sqlartan.core.stream.IterableStream;
-import sqlartan.view.AllRequestController;
-import sqlartan.view.tabs.struct.DatabaseStructure;
 import java.io.IOException;
 
 /**
  * Created by julien on 30.04.16.
  */
-public class DatabaseTabsController{
+public class DatabaseTabsController {
 
 	@FXML
 	private TableColumn<DatabaseStructure, String> colName;
@@ -85,88 +81,74 @@ public class DatabaseTabsController{
 		colType.setCellValueFactory(param -> param.getValue().typeProperty());
 		colRename.setCellFactory(new Callback<TableColumn<DatabaseStructure, String>, TableCell<DatabaseStructure, String>>() {
 			@Override
-			public TableCell call( final TableColumn<DatabaseStructure, String> param )
-			{
-				final TableCell<DatabaseStructure, String> cell = new TableCell<DatabaseStructure, String>()
-				{
-					final Button btn = new Button( "Rename" );
+			public TableCell<DatabaseStructure, String> call(final TableColumn<DatabaseStructure, String> param) {
+				return new TableCell<DatabaseStructure, String>() {
+					private final Button btn = new Button("Rename");
 
 					@Override
-					public void updateItem( String item, boolean empty )
-					{
-						super.updateItem( item, empty );
-						if ( empty )
-						{
-							setGraphic( null );
-							setText( null );
-						}
-						else
-						{
-							btn.setOnAction( ( ActionEvent event ) ->
+					public void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+						if (empty) {
+							setGraphic(null);
+							setText(null);
+						} else {
+							btn.setOnAction((ActionEvent event) ->
 							{
-								DatabaseStructure dbStruct = getTableView().getItems().get( getIndex() );
-								switch(dbStruct.typeProperty().get()){
-									case "View" :
-										Popup.input("Rename", "Rename " + dbStruct.nameProperty().get() + " into : ",  dbStruct.nameProperty().get()).ifPresent(name -> {
-											if (name.length() > 0 && ! dbStruct.nameProperty().get().equals(name)) {
+								DatabaseStructure dbStruct = getTableView().getItems().get(getIndex());
+								switch (dbStruct.typeProperty().get()) {
+									case "View":
+										Popup.input("Rename", "Rename " + dbStruct.nameProperty().get() + " into : ", dbStruct.nameProperty().get()).ifPresent(name -> {
+											if (name.length() > 0 && !dbStruct.nameProperty().get().equals(name)) {
 												controller.renameView(database.view(dbStruct.nameProperty().get()).get(), name);
 											}
 										});
 										break;
-									case "Table" :
-										Popup.input("Rename", "Rename " + dbStruct.nameProperty().get() + " into : ",  dbStruct.nameProperty().get()).ifPresent(name -> {
-											if (name.length() > 0 && ! dbStruct.nameProperty().get().equals(name)) {
+									case "Table":
+										Popup.input("Rename", "Rename " + dbStruct.nameProperty().get() + " into : ", dbStruct.nameProperty().get()).ifPresent(name -> {
+											if (name.length() > 0 && !dbStruct.nameProperty().get().equals(name)) {
 												controller.renameTable(database.table(dbStruct.nameProperty().get()).get(), name);
 											}
 										});
 										break;
 								}
-							} );
-							setGraphic( btn );
-							setText( null );
+							});
+							setGraphic(btn);
+							setText(null);
 						}
 					}
 				};
-				return cell;
 			}
 		});
 		colDelete.setCellFactory(new Callback<TableColumn<DatabaseStructure, String>, TableCell<DatabaseStructure, String>>() {
 			@Override
-			public TableCell call( final TableColumn<DatabaseStructure, String> param )
-			{
-				final TableCell<DatabaseStructure, String> cell = new TableCell<DatabaseStructure, String>()
-				{
-					final Button btn = new Button( "Drop" );
+			public TableCell<DatabaseStructure, String> call(final TableColumn<DatabaseStructure, String> param) {
+				return new TableCell<DatabaseStructure, String>() {
+					private final Button btn = new Button("Drop");
 
 					@Override
-					public void updateItem( String item, boolean empty )
-					{
-						super.updateItem( item, empty );
-						if ( empty )
-						{
-							setGraphic( null );
-							setText( null );
-						}
-						else
-						{
-							btn.setOnAction( ( ActionEvent event ) ->
+					public void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+						if (empty) {
+							setGraphic(null);
+							setText(null);
+						} else {
+							btn.setOnAction((ActionEvent event) ->
 							{
-								DatabaseStructure dbStruct = getTableView().getItems().get( getIndex() );
-								switch(dbStruct.typeProperty().get()){
-									case "View" :
+								DatabaseStructure dbStruct = getTableView().getItems().get(getIndex());
+								switch (dbStruct.typeProperty().get()) {
+									case "View":
 										controller.dropView(database.view(dbStruct.nameProperty().get()).get());
 										break;
-									case "Table" :
+									case "Table":
 										controller.dropStructure(database.table(dbStruct.nameProperty().get()).get());
 										break;
 								}
-							} );
-							setGraphic( btn );
-							setText( null );
+							});
+							setGraphic(btn);
+							setText(null);
 						}
 					}
 				};
-				return cell;
 			}
 		});
 
@@ -204,12 +186,12 @@ public class DatabaseTabsController{
 		this.controller = controller;
 	}
 
-	public void selectSqlTab(){
+	public void selectSqlTab() {
 		tabPane.getSelectionModel().selectFirst();
 		tabPane.getSelectionModel().selectNext();
 	}
 
-	public void setSqlRequest(String request){
+	public void setSqlRequest(String request) {
 		allRequestControler.setRequest(request);
 	}
 }
