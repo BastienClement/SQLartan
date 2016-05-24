c57a2cb56aa72776b77ea9ccb65e1e11c9fd9a4a
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-05-24 13:56:08 +0200

    Added TabStructure and ViewTabStructure

--- src/sqlartan/view/tabs/struct/DatabaseStructure.java
+++ src/sqlartan/view/tabs/struct/DatabaseStructure.java
@@ -15,34 +15,18 @@ import static sqlartan.util.Matching.match;
  *
  * Represente the structure tab for a database.
  */
-public class DatabaseStructure {
-	private StringProperty name;
-	private StringProperty type;
+public class DatabaseStructure extends TabStructure {
 	private StringProperty lignes;
-	private StringProperty actions;
 
 	public DatabaseStructure(PersistentStructure<?> structure) {
-		this.name = new SimpleStringProperty(structure.name());
-		this.type = new SimpleStringProperty(match(structure)
-				.when(Table.class, t -> "Table")
-				.when(View.class, v -> "View")
-				.orElse("Unknown"));
+		super(structure.name(), match(structure)
+			.when(Table.class, t -> "Table")
+			.when(View.class, v -> "View")
+			.orElse("Unknown"));
 		this.lignes = new SimpleStringProperty("0"); // TODO when nbLignes is done (Bastien will have slept enough)
-	}
-
-	public StringProperty nameProperty() {
-		return name;
-	}
-
-	public StringProperty typeProperty() {
-		return type;
 	}
 
 	public StringProperty lignesProperty() {
 		return lignes;
-	}
-
-	public StringProperty actionsProperty() {
-		return actions;
 	}
 }
---
+++ src/sqlartan/view/tabs/struct/ViewTabStructure.java
@@ -0,0 +1,10 @@
+package sqlartan.view.tabs.struct;
+
+/**
+ * Projet : SQLartan
+ * Créé le 24.05.2016.
+ *
+ * @author Adriano Ruberto
+ */
+public class ViewTabStructure {
+}
