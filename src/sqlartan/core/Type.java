7faa5daed330b2563186ec05406103fe064d2828
Bastien Clément <g@ledric.me>
2016-05-28 22:14:29 +0200

    Type.convert()

--- src/sqlartan/core/Type.java
+++ src/sqlartan/core/Type.java
@@ -1,12 +1,24 @@
 package sqlartan.core;
 
+import sqlartan.core.util.DataConverter;
+
 /**
  * Types of data supported by SQLite
  */
 public enum Type {
-	Null,
-	Integer,
-	Real,
-	Text,
-	Blob
+	Null(Object.class),
+	Integer(java.lang.Integer.class),
+	Real(Double.class),
+	Text(String.class),
+	Blob(Object.class);
+
+	private Class<?> javaType;
+
+	Type(Class<?> java) {
+		this.javaType = java;
+	}
+
+	public Object convert(Object from) {
+		return DataConverter.convert(from, javaType);
+	}
 }
